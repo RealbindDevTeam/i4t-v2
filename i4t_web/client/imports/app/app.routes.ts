915f268a23ec2f5c4da7d578771922b52ad36be1
Leonardo González <clgrhc@gmail.com>
2018-03-14 10:36:37 -0500

    removed web signup for consumer and redirect to download app

--- i4t_web/client/imports/app/app.routes.ts
+++ i4t_web/client/imports/app/app.routes.ts
@@ -8,7 +8,7 @@ import { SupervisorGuard } from './web/services/navigation/supervisor-guard.serv
 import { CashierGuard } from './web/services/navigation/cashier-guard.service';
 import { DashboardComponent } from './web/administrator/dashboard/dashboard.component';
 import { SectionComponent } from './web/administrator/menu/sections/section/section.component';
-import { SignupWebComponent } from './web/auth/signup/signup.web.component';
+//import { SignupWebComponent } from './web/auth/signup/signup.web.component';
 import { SigninWebComponent } from './web/auth/signin/signin.web.component';
 import { CategoryComponent } from './web/administrator/menu/categories/categories/categories.component';
 import { SubcategoryComponent } from './web/administrator/menu/subcategories/subcategories/subcategories.component';
@@ -116,7 +116,7 @@ export const routes: Route[] = [
         ]
     },
     { path: '', component: SigninWebComponent },
-    { path: 'signup', component: SignupWebComponent },
+    //{ path: 'signup', component: SignupWebComponent },
     { path: 'admin-signup', component: AdminSignupComponent },
     { path: 'reset-password/:tk', component: ResetPasswordWebComponent },
     { path: 'go-to-store/:ic', component: GoToStoreComponent },
